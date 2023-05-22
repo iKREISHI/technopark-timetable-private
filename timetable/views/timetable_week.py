615cf7d5d06f175fda03127eebe6e5fa82a8545c
shiro-av <vadas25@yandex.ru>
2023-05-22 14:07:49 +0500

    расписание по неделям в виде пон-воскр

---
+++ timetable/views/timetable_week.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.mixins import PermissionRequiredMixin, PermissionDenied, LoginRequiredMixin
+from django.views.generic import ListView
+from timetable.models.timetable import TimetableItem
+from users.models.university import Auditorium
+from datetime import datetime
+
+
+class TimetableWeekView(View):
+    template_name = 'timetable/schedule/week.html'
+
+    def get(self, request, monday, sunday):
+        mon = datetime.strptime(monday, '%d_%m_%y').date()
+        sun = datetime.strptime(sunday, '%d_%m_%y').date()
+        auditoriums = Auditorium.objects.all()
+        context = {
+            'title': f'Расписание на неделю {monday} - {sunday}',
+            'auditoriums': auditoriums,
+            'start_week': mon,
+            'end_week': sun,
+        }
+        return render(request, self.template_name, context)
