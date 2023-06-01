d9701cd90fca85436e881955f9dc507528a2588e
shiro-av <vadas25@yandex.ru>
2023-06-01 20:53:44 +0500

    add DRF to requirements.txt

--- timetable/urls.py
+++ timetable/urls.py
@@ -2,7 +2,9 @@ from django.urls import include, path
 from timetable.views.reservation import AddTimeTableReservation
 from timetable.views.user_reservations import UserReservationsList, UserReservationUpdateView
 from timetable.views.admin_reservations import AdminApproveReservationList, AdminApproveReservation
-from timetable.views.timetable_current_week import TimeTableCurrentWeekView
+from timetable.views.timetable_current_week import (
+    TimeTableCurrentWeekView,
+)
 from timetable.views.timetable_item_edit import TimeTableItemUpdateView
 from timetable.views.timetable_item_delete import TimeTableItemDeleteView
 from timetable.views.timetable_week import TimetableWeekView
@@ -20,6 +22,7 @@ urlpatterns = [
         AdminApproveReservation.as_view(), name='approve-reservation'
     ),
     path('current-week/', TimeTableCurrentWeekView.as_view(), name='current-week'),
+    # path('current-week-json/', TimeTableCurrentWeekJSONView.as_view(), name='current-week-json'),
     path('item/<int:pk>/update/', TimeTableItemUpdateView.as_view(), name='timetableitem-update'),
     path('item/<int:pk>/delete', TimeTableItemDeleteView.as_view(), name='timetableitem-delete'),
     path('week-<str:monday>-<str:sunday>', TimetableWeekView.as_view(), name='timetable-week'),
--- timetable/views/timetable_current_week.py
+++ timetable/views/timetable_current_week.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
+import json
 from django.contrib.auth.mixins import PermissionRequiredMixin, PermissionDenied, LoginRequiredMixin
 from django.views.generic import ListView
 from timetable.models.timetable import TimetableItem
@@ -27,3 +29,47 @@ class TimeTableCurrentWeekView(View):
 
     def post(self, request):
         pass
+
+
+class TimeTableCurrentWeekJSONView(View):
+    template_name = 'timetable/schedule/current_week.html'
+    model = TimetableItem
+
+    def get(self, request):
+        today = date.today()
+        start_week = today - timedelta(days=today.weekday())
+        end_week = start_week + timedelta(days=6)
+        auditoriums = Auditorium.objects.all()
+        data = []
+
+        for aud in auditoriums:
+            timetable_items = aud.timetableitem_set.filter(date__range=[start_week, end_week]).order_by('date',
+                                                                                                        'start_time')
+            items = []
+
+            for item in timetable_items:
+                if item.organazer.last_name and item.organazer.first_name:
+                    responsible = f"{item.organazer.last_name} {item.organazer.first_name}"
+                else:
+                    responsible = f"Пользователь: {item.organazer}"
+
+                items.append({
+                    'date': item.date,
+                    'start_time': item.start_time.strftime('%H:%M'),
+                    'end_time': item.end_time.strftime('%H:%M'),
+                    'name': item.name,
+                    'responsible': responsible
+                })
+
+            data.append({
+                'name': aud.name,
+                'items': items
+            })
+
+        context = {
+            'title': 'Расписание на текущую неделю',
+            'data': data
+        }
+
+        json_data = json.dumps(context)
+        return HttpResponse(json_data, content_type='application/json')
